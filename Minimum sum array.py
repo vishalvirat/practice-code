c271b9832aa4592176d445416658692e473c2f43
V M VISHAL <vishalvm2005@gmail.com>
2024-09-27 09:26:51 +0530

    Create Minimum sum array.py

---
+++ Minimum sum array.py
@@ -0,0 +1,14 @@
+def min_sum(arr):
+    arr.sort(reverse=True)
+    total=arr[0]
+    avg=arr[0]
+    for i in range(1,len(arr)):
+        if arr[i]<avg:
+          break
+        total+=arr[i]
+        avg=(total)/(1+1)
+    return total
+n=int(input())
+arr=list(map(int,input().split()))
+result=min_sum(arr)
+print(result)
